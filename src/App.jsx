1caf454b3cc39b7a09c7444e86de707072060d88
codernoyan <coder.noyan@gmail.com>
2023-01-30 01:30:33 +0600

    project initialized with material ui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { CssBaseline } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
